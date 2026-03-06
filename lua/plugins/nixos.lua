160261da8902fc1502070e51afdb8fe4b4dddab3
Moritz Schrauth <contact@mschrauth.de>
2026-03-06 11:08:32 +0100

    enable lua ls on nixos

--- lua/plugins/nixos.lua
+++ lua/plugins/nixos.lua
@@ -1,35 +1,39 @@
-
 local function is_nixos()
-  local f = io.open("/etc/os-release", "r")
-  if not f then
-    return false
-  end
-  local content = f:read("*a")
-  f:close()
-  return content:match("ID=nixos") ~= nil
+    local f = io.open("/etc/os-release", "r")
+    if not f then
+        return false
+    end
+    local content = f:read("*a")
+    f:close()
+    return content:match("ID=nixos") ~= nil
 end
 
 local function enable_lsp(lsp, opts)
-  opts.servers[lsp] = opts.servers[lsp] or {}
-  opts.servers[lsp].enabled = true
+    opts.servers[lsp] = opts.servers[lsp] or {}
+    opts.servers[lsp].enabled = true
 end
 
 local on_nixos = is_nixos()
 
-return {
-    { "mason-org/mason.nvim", enabled = false },
-    { "mason-org/mason-lspconfig.nvim", enabled = false },
-    {
-        "neovim/nvim-lspconfig",
-        opts = function(_, opts)
-            opts.servers = opts.servers or {}
+if on_nixos then
+    return {
+        { "mason-org/mason.nvim",           enabled = false },
+        { "mason-org/mason-lspconfig.nvim", enabled = false },
+        {
+            "neovim/nvim-lspconfig",
+            opts = function(_, opts)
+                opts.servers = opts.servers or {}
 
-            enable_lsp("bashls", opts)
-            enable_lsp("clangd", opts)
-            enable_lsp("nix_ls", opts)
-            enable_lsp("ocamllsp", opts)
-            enable_lsp("pyright", opts)
-            return opts
-        end,
+                enable_lsp("bashls", opts)
+                enable_lsp("clangd", opts)
+                enable_lsp("nix_ls", opts)
+                enable_lsp("ocamllsp", opts)
+                enable_lsp("pyright", opts)
+                enable_lsp("lua_ls", opts)
+                return opts
+            end,
+        }
     }
-}
+else
+    return {}
+end
